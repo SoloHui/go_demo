6ffdcd6ad2ae0e033009738bdaf8cd2160b7a8ae
Jinghui Liang <3414467112@qq.com>
2025-12-25 16:57:40 +0800

    feat(for-continued): 添加 Go 语言 for 循环示例代码（续）

---
+++ 03_flowcontrol/02_for-continued/main.go
@@ -0,0 +1,22 @@
+﻿/*
+---
+title: Go 语言之旅
+url: https://tour.go-zh.org/flowcontrol/2
+publishedTime: undefined
+---
+
+## for 循环（续）
+
+初始化语句和后置语句是可选的。
+*/
+package main
+
+import "fmt"
+
+func main() {
+	sum := 1
+	for ; sum < 1000; {
+		sum += sum
+	}
+	fmt.Println(sum)
+}
