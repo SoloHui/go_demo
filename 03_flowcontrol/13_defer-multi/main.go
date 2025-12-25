b2d22ef4c1e9bd5fe13f9e282ccdde0198e3cc8e
Jinghui Liang <3414467112@qq.com>
2025-12-25 17:00:26 +0800

    feat(defer): 添加 Go 语言 defer 栈示例代码

---
+++ 03_flowcontrol/13_defer-multi/main.go
@@ -0,0 +1,30 @@
+﻿/*
+---
+title: Go 语言之旅
+url: https://tour.go-zh.org/flowcontrol/13
+publishedTime: undefined
+---
+
+## defer 栈
+
+推迟调用的函数调用会被压入一个栈中。
+
+当外层函数返回时，被推迟的调用会按照后进先出的顺序调用。
+
+更多关于 defer 语句的信息，
+
+请阅读[此博文](http://blog.go-zh.org/defer-panic-and-recover)。
+*/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("counting")
+
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+
+	fmt.Println("done")
+}
