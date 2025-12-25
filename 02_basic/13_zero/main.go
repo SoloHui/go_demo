dc2b58f4ec45116c14bbdf8e10c3a062735f6ed5
Jinghui Liang <3414467112@qq.com>
2025-12-25 15:45:56 +0800

    feat(zero-values): 添加 Go 语言零值示例程序

---
+++ 02_basic/13_zero/main.go
@@ -0,0 +1,30 @@
+﻿/*
+---
+title: Go 语言之旅
+url: https://tour.go-zh.org/basics/12
+publishedTime: undefined
+---
+
+## 零值
+
+没有明确初始化的变量声明会被赋予对应类型的 **零值**。
+
+零值是：
+
+- 数值类型为 `0`，
+- 布尔类型为 `false`，
+- 字符串为 `""`（空字符串）。
+*/
+package main
+
+import "fmt"
+
+func main() {
+	var i int
+	var f float64
+	var b bool
+	var s string
+	// `%v` 以默认格式打印值
+	// `%q` 输出带双引号的字符串
+	fmt.Printf("%v %v %v %q\n", i, f, b, s)
+}
