2675f4641b66dffe3054282ce32fc455b8507efe
Jinghui Liang <3414467112@qq.com>
2025-12-25 15:46:19 +0800

    feat(constants): 添加 Go 语言常量示例程序

---
+++ 02_basic/16_constants/main.go
@@ -0,0 +1,29 @@
+﻿/*
+---
+title: Go 语言之旅
+url: https://tour.go-zh.org/basics/15
+publishedTime: undefined
+---
+
+## 常量
+
+常量的声明与变量类似，只不过使用`const`关键字。
+
+常量可以是字符、字符串、布尔值或数值。
+
+常量不能用`:=`语法声明。
+*/
+package main
+
+import "fmt"
+
+const Pi float32 = 3.14
+
+func main() {
+	const World = "世界"
+	fmt.Println("Hello", World)
+	fmt.Println("Happy", Pi, "Day")
+
+	const Truth = true
+	fmt.Println("Go rules?", Truth)
+}
