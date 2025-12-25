d2f83acc4bf2c64b59b0fc30ed8734ec960d2cbc
Jinghui Liang <3414467112@qq.com>
2025-12-25 15:44:49 +0800

    feat(function): 添加 Go 语言函数示例程序（续）

---
+++ 02_basic/06_functions-continued/main.go
@@ -0,0 +1,35 @@
+﻿/*
+---
+title: Go 语言之旅
+url: https://tour.go-zh.org/basics/5
+publishedTime: undefined
+---
+
+## 函数（续）
+
+当连续两个或多个函数的已命名形参类型相同时，除最后一个类型以外，其它都可以省略。
+
+在本例中，
+
+```
+x int, y int
+```
+
+被简写为
+
+```
+x, y int
+```
+*/
+package main
+
+import "fmt"
+
+// 函数结构: func 名字(参数 列表) 返回值 列表
+func add(x, y int) int {
+	return x + y
+}
+
+func main() {
+	fmt.Println(add(42, 13))
+}
