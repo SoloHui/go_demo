83ce5a8881beecc6cc678cd8e426a52b11395dbc
Jinghui Liang <3414467112@qq.com>
2025-12-25 15:44:56 +0800

    feat(multiple-results): 添加 Go 语言多重返回值示例程序

---
+++ 02_basic/07_multiple-results/main.go
@@ -0,0 +1,36 @@
+﻿/*
+---
+title: Go 语言之旅
+url: https://tour.go-zh.org/basics/6
+publishedTime: undefined
+---
+
+## 多重返回值
+
+Go 的函数可以返回任意数量的返回值。
+
+`swap` 函数接受两个字符串参数，并返回它们的值互换后的结果。
+
+```go
+func swap(x, y string) (string, string) {
+```
+*/
+package main
+
+import "fmt"
+
+// swap 函数返回两个字符串值
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func main() {
+	// 符号 `:=` 用于声明并初始化变量。
+	// 这里声明了两个变量 a 和 b 来接收 swap 函数的两个返回值。
+	a, b := swap("hello", "world")
+	fmt.Printf("a=%s b=%s\n", a, b)
+
+	// 符号 `=` 用于给已经声明的变量赋值。
+	a, b = swap("goodbye", "world")
+	fmt.Printf("a=%s b=%s\n", a, b)
+}
