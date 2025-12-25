ac8775f9e41133712583196168a51fc0c3702e95
Jinghui Liang <3414467112@qq.com>
2025-12-25 15:45:21 +0800

    feat(variables): 添加 Go 语言变量示例程序

---
+++ 02_basic/09_variables/main.go
@@ -0,0 +1,23 @@
+﻿/*
+---
+title: Go 语言之旅
+url: https://tour.go-zh.org/basics/8
+publishedTime: undefined
+---
+
+## 变量
+
+`var` 语句用于声明一系列变量。和函数的参数列表一样，类型在最后。
+
+如例中所示，`var` 语句可以出现在包或函数的层级。
+*/
+package main
+
+import "fmt"
+
+var c, python, java bool
+
+func main() {
+	var i int
+	fmt.Println(i, c, python, java)
+}
