8602db5bab605942899433ff45d570db779e2101
Jinghui Liang <3414467112@qq.com>
2025-12-26 16:03:45 +0800

    feat(struct-fields): 添加 Go 语言结构体字段示例代码及说明

---
+++ 04_moretypes/03_struct-fields/main.go
@@ -0,0 +1,26 @@
+﻿/*
+---
+title: Go 语言之旅
+url: https://tour.go-zh.org/moretypes/3
+publishedTime: undefined
+---
+
+## 结构体字段
+
+结构体字段可以通过 `.` 访问。
+*/
+package main
+
+import "fmt"
+
+type Vertex struct {
+	X int
+	Y int
+}
+
+func main() {
+	v := Vertex{1, 2}
+	v.X = 4
+	v.Y = 5
+	fmt.Println(v.X)
+}
