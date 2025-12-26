3c77edc179f453defdc3a410a31008fe13f94e1d
Jinghui Liang <3414467112@qq.com>
2025-12-26 16:03:39 +0800

    feat(struct): 添加 Go 语言结构体示例代码及说明

---
+++ 04_moretypes/02_struct/main.go
@@ -0,0 +1,24 @@
+﻿/*
+---
+title: Go 语言之旅
+url: https://tour.go-zh.org/moretypes/2
+publishedTime: undefined
+---
+
+## 结构体
+
+一个 结构体（`struct`）就是一组 字段（field）。
+*/
+package main
+
+import "fmt"
+
+// 关键字 `type` 用于定义新的类型，`struct` 用于定义结构体。
+type Vertex struct {
+	X int
+	Y int
+}
+
+func main() {
+	fmt.Println(Vertex{1, 2})
+}
