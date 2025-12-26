a0939d71f3824f0712476c1009511b8fae664286
Jinghui Liang <3414467112@qq.com>
2025-12-26 16:06:54 +0800

    feat(nil-slices): 添加 Go 语言 nil 切片示例代码及说明

---
+++ 04_moretypes/12_nil-slices/main.go
@@ -0,0 +1,23 @@
+﻿/*
+---
+title: Go 语言之旅
+url: https://tour.go-zh.org/moretypes/12
+publishedTime: undefined
+---
+
+## nil 切片
+切片的零值是`nil`。
+
+`nil`切片的长度和容量为 0 且没有底层数组。
+*/
+package main
+
+import "fmt"
+
+func main() {
+	var s []int
+	fmt.Println(s, len(s), cap(s))
+	if s == nil {
+		fmt.Println("nil!")
+	}
+}
