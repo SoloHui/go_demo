b32eb188a9b8b9fce5ec736fc366a6d93db5a88e
Jinghui Liang <3414467112@qq.com>
2025-12-29 08:51:10 +0800

    feat(methods): 添加接口值示例代码及相关说明

---
+++ 05_methods/11_interface-values/main.go
@@ -0,0 +1,64 @@
+﻿/*
+\---
+title: Go 语言之旅
+url: https://tour.go-zh.org/methods/11
+publishedTime: undefined
+---
+
+## 接口值
+
+接口也是值。它们可以像其它值一样传递。
+
+接口值可以用作函数的参数或返回值。
+
+在内部，接口值可以看做包含值和具体类型的元组：
+
+```javascript
+(value, type)
+```
+
+接口值保存了一个具体底层类型的具体值。
+
+接口值调用方法时会执行其底层类型的同名方法。
+*/
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type I interface {
+	M()
+}
+
+type T struct {
+	S string
+}
+
+func (t *T) M() {
+	fmt.Println(t.S)
+}
+
+type F float64
+
+func (f F) M() {
+	fmt.Println(f)
+}
+
+func main() {
+	var i I
+
+	i = &T{"Hello"}
+	describe(i)
+	i.M()
+
+	i = F(math.Pi)
+	describe(i)
+	i.M()
+}
+
+func describe(i I) {
+	// 这里我们使用了 `%v` 和 `%T` 两个格式化动词来打印接口值的具体值和具体类型。
+	fmt.Printf("(%v, %T)\n", i, i)
+}
