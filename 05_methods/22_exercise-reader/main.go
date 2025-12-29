120b481e60b97d41d037fd0a2619951171dde432
Jinghui Liang <3414467112@qq.com>
2025-12-29 09:00:24 +0800

    feat(reader): 实现 MyReader 类型及其 Read 方法，生成 ASCII 字符 'A' 的无限流

---
+++ 05_methods/22_exercise-reader/main.go
@@ -0,0 +1,28 @@
+﻿/*
+---
+title: Go 语言之旅
+url: https://tour.go-zh.org/methods/22
+publishedTime: undefined
+---
+
+## 练习：Reader
+
+实现一个 `Reader`，它产生 ASCII 字符 'A'的无限流。
+*/
+package main
+
+import "golang.org/x/tour/reader"
+
+type MyReader struct{}
+
+// TODO: 为 MyReader 添加一个 Read([]byte) (int, error) 方法。
+func (r MyReader) Read(b []byte) (int, error) {
+	for i := range b {
+		b[i] = 'A'
+	}
+	return len(b), nil
+}
+
+func main() {
+	reader.Validate(MyReader{})
+}
