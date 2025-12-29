7d05a0c1989c9bcc506b0f51f779d0431b72c76d
Jinghui Liang <3414467112@qq.com>
2025-12-29 08:55:17 +0800

    feat(methods): 添加 IPAddr 类型及其 String 方法示例代码

---
+++ 05_methods/18_exercise-stringer/main.go
@@ -0,0 +1,32 @@
+﻿/*
+---
+title: Go 语言之旅
+url: https://tour.go-zh.org/methods/18
+publishedTime: undefined
+---
+
+## 练习：Stringer
+通过让`IPAddr`类型实现`fmt.Stringer`来打印点号分隔的地址。
+
+例如，`IPAddr{1, 2, 3, 4}`应当打印为 `"1.2.3.4"`。
+*/
+package main
+
+import "fmt"
+
+type IPAddr [4]byte
+
+// TODO: 为 IPAddr 添加一个 "String() string" 方法。
+func (ip IPAddr) String() string {
+	return fmt.Sprintf("%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3])
+}
+
+func main() {
+	hosts := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+	for name, ip := range hosts {
+		fmt.Printf("%v: %v\n", name, ip)
+	}
+}
