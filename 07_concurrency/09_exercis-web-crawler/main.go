5f21c2db8f085255f0756bb3e8b5415340db83e7
Jinghui Liang <3414467112@qq.com>
2025-12-29 14:33:42 +0800

    feat(concurrency): 实现并行化 Web 爬虫功能

---
+++ 07_concurrency/09_exercis-web-crawler/main.go
@@ -0,0 +1,124 @@
+﻿/*
+---
+title: Go 语言之旅
+url: https://tour.go-zh.org/concurrency/10
+publishedTime: undefined
+---
+
+## 练习：Web 爬虫
+
+在这个练习中，我们将会使用 Go 的并发特性来并行化一个 Web 爬虫。
+
+修改 Crawl 函数来并行地抓取 URL，并且保证不重复。
+
+*提示：* 你可以用一个 map 来缓存已经获取的 URL，但是要注意 map 本身并不是并发安全的！
+*/
+package main
+
+import (
+	"fmt"
+)
+
+type Fetcher interface {
+	// Fetch 返回 URL 所指向页面的 body 内容，
+	// 并将该页面上找到的所有 URL 放到一个切片中。
+	Fetch(url string) (body string, urls []string, err error)
+}
+
+// Crawl 用 fetcher 从某个 URL 开始递归的爬取页面，直到达到最大深度。
+func Crawl(url string, depth int, fetcher Fetcher) {
+	// TODO: 并行地爬取 URL。
+	ch := make(chan struct{})
+	var crawl func(string, int)
+	crawl = func(url string, depth int) {
+		// `defer` 用于在函数返回时发送信号到通道 ch
+		defer func() { ch <- struct{}{} }()
+		if depth <= 0 {
+			return
+		}
+		body, urls, err := fetcher.Fetch(url)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("found: %s %q\n", url, body)
+		for _, u := range urls {
+			go crawl(u, depth-1)
+		}
+	}
+	go crawl(url, depth)
+	// 等待所有的爬取工作完成
+	for i := 0; i < 1<<depth-1; i++ {
+		<-ch
+	}
+
+	// TODO: 不重复爬取页面。
+
+	// 下面并没有实现上面两种情况：
+	// if depth <= 0 {
+	// 	return
+	// }
+	// body, urls, err := fetcher.Fetch(url)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// fmt.Printf("found: %s %q\n", url, body)
+	// for _, u := range urls {
+	// 	Crawl(u, depth-1, fetcher)
+	// }
+	// return
+}
+
+func main() {
+	Crawl("https://golang.org/", 4, fetcher)
+}
+
+// fakeFetcher 是待填充结果的 Fetcher。
+type fakeFetcher map[string]*fakeResult
+
+type fakeResult struct {
+	body string
+	urls []string
+}
+
+func (f fakeFetcher) Fetch(url string) (string, []string, error) {
+	if res, ok := f[url]; ok {
+		return res.body, res.urls, nil
+	}
+	return "", nil, fmt.Errorf("not found: %s", url)
+}
+
+// fetcher 是填充后的 fakeFetcher。
+var fetcher = fakeFetcher{
+	"https://golang.org/": &fakeResult{
+		"The Go Programming Language",
+		[]string{
+			"https://golang.org/pkg/",
+			"https://golang.org/cmd/",
+		},
+	},
+	"https://golang.org/pkg/": &fakeResult{
+		"Packages",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/cmd/",
+			"https://golang.org/pkg/fmt/",
+			"https://golang.org/pkg/os/",
+		},
+	},
+	"https://golang.org/pkg/fmt/": &fakeResult{
+		"Package fmt",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/pkg/",
+		},
+	},
+	"https://golang.org/pkg/os/": &fakeResult{
+		"Package os",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/pkg/",
+		},
+	},
+}
